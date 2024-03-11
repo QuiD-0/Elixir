b4b3079e5b9db592733c4eeb5609e2c628e0b12b
QuiD-0 <wodnd101@naver.com>
2024-03-11 10:10:08 +0900

    type 명시

--- basic/lang/Struct.ex
+++ basic/lang/Struct.ex
@@ -1,11 +1,23 @@
+defmodule UserName do
+  @type t :: %UserName{first_name: String.t(), last_name: String.t()}
+  defstruct [:first_name, :last_name]
+
+  def new(first_name, last_name) do
+    %UserName{first_name: first_name, last_name: last_name}
+  end
+end
+
 defmodule User do
-  defstruct name: "",
-            age: 0,
-            email: "",
-            admin: false
+  @type t :: %User{
+          name: %UserName{},
+          age: non_neg_integer(),
+          email: String.t(),
+          admin: boolean()
+        }
+  defstruct [:name, :age, :email, :admin]
 
   def new(name, age, email) do
-    %User{name: name, age: age, email: email}
+    %User{name: name, age: age, email: email, admin: false}
   end
 
   def rename(user, new_name) do
@@ -21,9 +33,9 @@ defmodule User do
   end
 end
 
-User.new("quid", 10, "")
+User.new(UserName.new("test", "name"), 10, "test@mail.com")
 |> IO.inspect()
-|> User.rename("quid2")
+|> User.rename(UserName.new("new", "name"))
 |> IO.inspect()
 |> User.make_admin()
 |> IO.inspect()
