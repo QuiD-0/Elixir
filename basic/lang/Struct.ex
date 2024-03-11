13900ca6e7a52afec23bc744010f33280ff2df4d
QuiD-0 <wodnd101@naver.com>
2024-03-11 16:19:26 +0900

    struct 타입 체크

--- basic/lang/Struct.ex
+++ basic/lang/Struct.ex
@@ -1,27 +1,25 @@
 defmodule UserName do
-  @type t :: %UserName{first_name: String.t(), last_name: String.t()}
   defstruct [:first_name, :last_name]
 
-  def new(first_name, last_name) do
+  def new(first_name, last_name) when is_binary(first_name) and is_binary(last_name) do
     %UserName{first_name: first_name, last_name: last_name}
+  end
+
+  def new(first_name, last_name) do
+    raise ArgumentError, "First and last name must be strings"
   end
 end
 
 defmodule User do
-  @type t :: %User{
-          name: %UserName{},
-          age: non_neg_integer(),
-          email: String.t(),
-          admin: boolean()
-        }
   defstruct [:name, :age, :email, :admin]
 
-  def new(name, age, email) do
+  def new(name, age, email)
+      when is_struct(name, UserName) and is_integer(age) and is_binary(email) do
     %User{name: name, age: age, email: email, admin: false}
   end
 
-  def rename(user, new_name) do
-    %{user | name: new_name}
+  def rename(user, %{first_name: first, last_name: last}) do
+    %{user | name: UserName.new(first, last)}
   end
 
   def make_admin(user) do
@@ -33,7 +31,7 @@ defmodule User do
   end
 end
 
-User.new(UserName.new("test", "name"), 10, "test@mail.com")
+User.new(UserName.new("John", "Doe"), 30, "email")
 |> IO.inspect()
 |> User.rename(UserName.new("new", "name"))
 |> IO.inspect()
