62da762aaee6bdd72972c9da067fe48ae5a45d87
QuiD-0 <wodnd101@naver.com>
2024-03-13 10:38:49 +0900

    protocol for struct

--- basic/lang/Protocol.ex
+++ basic/lang/Protocol.ex
@@ -1,18 +1,37 @@
 defprotocol Utility do
-  @spec type(t) :: String.t()
   def type(value)
 end
 
 defimpl Utility, for: BitString do
-  def type(_value), do: "string"
+  def type(value), do: "hello " <> value
 end
 
 defimpl Utility, for: Integer do
-  def type(_value), do: "integer"
+  def type(value), do: "integer: " <> Integer.to_string(value)
+end
+
+defimpl Utility, for: List do
+  def type(value), do: value |> Enum.join(", ")
 end
 
 Utility.type("foo")
 |> IO.puts()
 
 Utility.type(42)
+|> IO.puts()
+
+Utility.type([1, 2, 3])
+|> IO.puts()
+
+defmodule User do
+  defstruct [:name, :age]
+  def new(name, age), do: %User{name: name, age: age}
+end
+
+defimpl Utility, for: User do
+  def type(%User{name: name, age: age}), do: "User: #{name} (#{age})"
+end
+
+User.new("John Doe", 42)
+|> Utility.type()
 |> IO.puts()
