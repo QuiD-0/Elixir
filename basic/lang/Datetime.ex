ce2232f17c18594653a760c9542d9e688eee79ee
quid-0 <wodnd101@naver.com>
2024-08-28 14:13:44 +0900

    Time

--- basic/lang/Datetime.ex
+++ basic/lang/Datetime.ex
@@ -1,3 +1,8 @@
+defmodule Is2024Year do
+  def check(%DateTime{year: 2024}), do: "Is 2024 year"
+  def check(_), do: "Is not 2024 year"
+end
+
 DateTime.utc_now()
 |> IO.inspect()
 
@@ -10,11 +15,27 @@ DateTime.utc_now()
 |> Date.add(10)
 |> IO.inspect()
 
-startDate = ~D[2020-01-01]
-endDate = ~D[2020-07-10]
+startDate = ~D[2020-06-01]
+endDate = ~D[2020-07-01]
 
 Date.diff(endDate, startDate)
 |> IO.inspect()
 
 Date.range(startDate, endDate)
-|> Enum.each(&IO.inspect/1)
+|> Enum.each(&IO.inspect(&1))
+
+Time.utc_now()
+|> IO.inspect()
+
+Time.add(Time.utc_now(), 10)
+|> IO.inspect()
+
+Time.new(23, 59, 59)
+|> IO.inspect()
+
+NaiveDateTime.utc_now()
+|> IO.inspect()
+
+DateTime.utc_now()
+|> Is2024Year.check()
+|> IO.inspect()
