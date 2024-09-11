a461ffec3f1fe3c9b72a6c46af7c7980e62b25a4
quid-0 <wodnd101@naver.com>
2024-09-11 17:35:13 +0900

    enum

--- basic/lang/Enum.ex
+++ basic/lang/Enum.ex
@@ -29,3 +29,63 @@ Command.now()
 [1, 2, 3, 4]
 |> Enum.map(&(&1 * 2))
 |> IO.inspect()
+
+Enum.to_list(1..5)
+|> IO.inspect()
+
+Enum.reduce(1..5, 0, &+/2)
+|> IO.inspect()
+
+Enum.concat([1, 2, 3], [4, 5, 6])
+|> IO.inspect()
+
+Enum.filter(1..10, &(&1 > 5))
+|> IO.inspect()
+
+Enum.sort([3, 1, 2])
+|> IO.inspect()
+
+Enum.sort(["cat", "apple", "bird"])
+|> IO.inspect()
+
+Enum.sort(["cat", "apple", "bird"], &(String.length(&1) < String.length(&2)))
+|> IO.inspect()
+
+Enum.max([3, 1, 2])
+|> IO.inspect()
+
+Enum.max_by(["cat", "apple", "bird"], &String.length/1)
+|> IO.inspect()
+
+Enum.all?([1, 2, 3], &(&1 > 0))
+|> IO.inspect()
+
+Enum.any?([1, 2, 3], &(&1 > 2))
+|> IO.inspect()
+
+Enum.chunk_every([1, 2, 3, 4, 5, 6, 7], 2)
+|> IO.inspect()
+
+Enum.join(["cat", "apple", "bird"], ", ")
+|> IO.puts()
+
+Enum.zip([1, 2, 3], [:a, :b, :c])
+|> IO.inspect()
+
+Enum.with_index([:a, :b, :c])
+|> IO.inspect()
+
+Enum.with_index([:a, :b, :c], 10)
+|> IO.inspect()
+
+deck =
+  Enum.map([~c"A", ~c"B", ~c"C", ~c"D"], fn suit ->
+    Enum.map(1..13, fn rank ->
+      {suit, rank}
+    end)
+  end)
+  |> List.flatten()
+
+Enum.shuffle(deck)
+|> Enum.take(5)
+|> IO.inspect()
